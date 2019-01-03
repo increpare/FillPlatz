f27a4c150b0206e87f9d962f3b92658baad4743c
Stephen Lavelle <analytic@gmail.com>
2019-01-03 17:03:40 +0100

    init

---
+++ genletters.js
@@ -0,0 +1,11 @@
+const TextToSVG = require('text-to-svg');
+const textToSVG = TextToSVG.loadSync();
+ 
+const attributes = {fill: 'red', stroke: 'black'};
+const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+ 
+const svg = textToSVG.getSVG('hello', options);
+
+var array = 
+console.log(svg);
+
